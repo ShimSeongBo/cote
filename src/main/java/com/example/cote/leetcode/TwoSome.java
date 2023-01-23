a9e033a1470546bb04404b251b0e1a161b231ff8
shimseongbo <shimseongbo@icloud.com>
2023-01-23 13:56:10 +0900

    Jan22 23:08

--- src/main/java/com/example/cote/leetcode/TwoSome.java
+++
@@ -1,4 +0,0 @@
-package com.example.cote.leetcode;
-
-public class TwoSome {
-}
---
+++ src/main/java/com/example/cote/leetcode/TwoSum.java
@@ -0,0 +1,14 @@
+package com.example.cote.leetcode;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ret = new int[2];
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+
+        }
+
+        return
+    }
+
+}
